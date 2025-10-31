911756c97905d953448d5951b4fb1bccd3fa1afe
Gabriela Aguirre <gvaguirres@gmail.com>
2025-10-31 10:38:55 +0100

    första chatens utseende

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,7 +1,12 @@
 package com.example;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * Controller layer: mediates between the view (FXML) and the model.
@@ -11,12 +16,27 @@ public class HelloController {
     private final HelloModel model = new HelloModel();
 
     @FXML
-    private Label messageLabel;
+    private TextArea chatArea;
+    @FXML
+    private TextField messageField;
+    @FXML
+    private Button sendButton;
+
+    @FXML
+    public AnchorPane rootPane;
 
     @FXML
     private void initialize() {
-        if (messageLabel != null) {
-            messageLabel.setText(model.getGreeting());
+
+    }
+
+
+    public void onSendButtonClicked(ActionEvent actionEvent) {
+        String message = messageField.getText().trim();
+        if (!message.isEmpty()) {
+            chatArea.appendText("You: " + message + "\n");
+            messageField.clear();
         }
+        //regNumberField.setStyle("-fx-border-color: red;");
     }
 }
